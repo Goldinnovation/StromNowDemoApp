5d04b84fed04588fb852feeecd539e018f06ab9b
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2026-02-04 23:04:45 +0100

    configured the eas build

--- app/(tabs)/Map.tsx
+++ app/(tabs)/Map.tsx
@@ -1,16 +1,35 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Mapbox from '@rnmapbox/maps';
 
 
 
+Mapbox.setAccessToken('<YOUR_ACCESSTOKEN>');
 
 const MabTab = () => {
     return (
-        <View className='flex-1 items-center justify-center bg-red-500'>
-            <Text className='text-white'>Map</Text>
-        </View>
+        <View style={styles.page}>
+      <View style={styles.container}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
+    </View>
     );
 };
 
+
+const styles = StyleSheet.create({
+    page: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container: {
+      height: 300,
+      width: 300,
+    },
+    map: {
+      flex: 1
+    }
+  });
 
 
 
